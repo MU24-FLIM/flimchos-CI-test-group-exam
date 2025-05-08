195370859d10b918b083f9cbdf7030e52399d9bc
Linda Blomkvist <3tomu24.blomli@folkuniversitetet.nu>
2025-05-08 15:07:40 +0200

    add CRUD to Restaurant classes

--- src/main/java/com/example/flimchos/model/Restaurant.java
+++ src/main/java/com/example/flimchos/model/Restaurant.java
@@ -1,4 +1,53 @@
 package com.example.flimchos.model;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String city;
+    //private int bookingId;
+
+    public Restaurant() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+//    public int getBookingId() {
+//        return bookingId;
+//    }
+//
+//    public void setBookingId(int bookingId) {
+//        this.bookingId = bookingId;
+//    }
 }
