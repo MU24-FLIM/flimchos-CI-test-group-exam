4bf65d4697b8de826ca1cdd0bc32d1823ed94317
Linda Blomkvist <3tomu24.blomli@folkuniversitetet.nu>
2025-05-09 14:09:54 +0200

    Remove restaurantDTO, adjust URL, PASSWORD and USER in applications

---
+++ src/main/java/com/example/flimchos/dto/RestaurantDTO.java
@@ -0,0 +1,50 @@
+//package com.example.flimchos.dto;
+//
+//public class RestaurantDTO {
+//    private Long id;
+//    private String email;
+//    private String city;
+//    private List<BookingDTO> bookings;
+//
+//    public RestaurantDTO(Long id, String email, String city, List<BookingDTO> bookings) {
+//        this.id = id;
+//        this.email = email;
+//        this.city = city;
+//        this.bookings = bookings;
+//    }
+//
+//    public RestaurantDTO() {
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//
+//    public List<BookingDTO> getBookings() {
+//        return bookings;
+//    }
+//
+//    public void setBookings(List<BookingDTO> bookings) {
+//        this.bookings = bookings;
+//    }
+//}
