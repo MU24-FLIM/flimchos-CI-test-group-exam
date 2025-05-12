6fb3f53a2c1b5b570809d6c223228337b08e2302
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-05-12 16:01:32 +0200

    add mapper

---
+++ src/main/java/com/example/flimchos/mapper/BookingMapper.java
@@ -0,0 +1,20 @@
+package com.example.flimchos.mapper;
+
+import com.example.flimchos.dto.BookingCreationDTO;
+import com.example.flimchos.dto.BookingDTO;
+import com.example.flimchos.model.Booking;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookingMapper {
+
+    BookingMapper INSTANCE = Mappers.getMapper(BookingMapper.class);
+
+    @Mapping(source = "guest.name", target = "guestName")
+    @Mapping(source = "restaurant.city", target = "restaurantCity")
+    BookingDTO bookingToBookingDTO(Booking booking);
+
+    Booking bookingCreationDTOToBooking(BookingCreationDTO bookingCreationDTO);
+}
