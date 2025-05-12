172cea3133e341f5292eb9498a50bc7bab005d1c
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-05-12 10:03:22 +0200

    fix tests to match new relationships

--- src/test/java/com/example/flimchos/controller/BookingControllerComponentTestWithMockMvc.java
+++ src/test/java/com/example/flimchos/controller/BookingControllerComponentTestWithMockMvc.java
@@ -1,9 +1,11 @@
 package com.example.flimchos.controller;
 
 import com.example.flimchos.model.Booking;
+import com.example.flimchos.model.Guest;
+import com.example.flimchos.model.Restaurant;
 import com.example.flimchos.repository.BookingRepository;
+import com.example.flimchos.repository.RestaurantRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,13 +16,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -32,6 +34,10 @@ class BookingControllerComponentTestWithMockMvc {
 
     @Autowired
     private BookingRepository bookingRepository;
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+    @Autowired
+    private BookingRepository guestRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -39,17 +45,29 @@ class BookingControllerComponentTestWithMockMvc {
     @Test
     public void testCreateBookingShouldReturnStatusOkAndBooking() throws Exception {
         //Arrange
+        Guest guest = new Guest("Madde", "<EMAIL>");
+        Restaurant restaurant = new Restaurant("<EMAIL>", "Linda", new ArrayList<>());
         LocalDate date = LocalDate.of(2025,11,01);
         LocalTime time = LocalTime.of(15, 00);
-        Booking booking = new Booking(date, time, 5, 1, 1);
+        Booking booking = new Booking(date, time, 5, guest, restaurant);
 
         //Act & Assert
-        mockMvc.perform(post("http://localhost:8080/booking")
+        mockMvc.perform(post("http://localhost:8080/restaurants")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(restaurant)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("http://localhost:8080/guest")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(guest)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("http://localhost:8080/booking/1/1")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(objectMapper.writeValueAsString(booking)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.date", is("2025-11-01")))
-                .andExpect(jsonPath("$.guestId", is(1)))
+                .andExpect(jsonPath("$.guest.id", is(1)))
                 .andDo(print());
     }
 
