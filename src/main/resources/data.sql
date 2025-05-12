3ff81e5d3a549ad9fed165a2b0c9cce5c8d935c4
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-05-12 13:47:12 +0200

    add preloaded data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO guest (name, email) VALUES('Ned', 'ned@ned.ned'), ('Liz', 'liz@liz.liz');
+INSERT IGNORE INTO restaurants (city, email) VALUES('Storstan', 'ned@ned.ned'), ('Lillstan', 'liz@liz.liz');
